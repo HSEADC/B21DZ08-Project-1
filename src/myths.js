bc088be77a5ae5560f2f679763d34d4f391b41df
DemichevaLiza <demicheva.liza2014@gmail.com>
2023-03-27 16:06:36 +0300

    Components

--- src/myths.js
+++ src/myths.js
@@ -51,56 +51,86 @@ function getZodiakNames() {
   }
 }
 
-function updatedContent() {
-  const contentCenterImage = document.querySelectorAll('.A_ZodiakInCircle')
-  const contentNameTitle = document.querySelectorAll('.A_ZodiakName')
-  const contentDescription = document.querySelectorAll('.M_ArticleRightBlock')
-  const contentPage = {
-    id: '',
-    image: contentCenterImage,
-    title: contentNameTitle,
-    description: contentDescription
-  }
+// function updatedContent() {
+//   const contentCenterImage = document.querySelectorAll('.A_ZodiakInCircle')
+//   const contentNameTitle = document.querySelectorAll('.A_ZodiakName')
+//   const contentDescription = document.querySelectorAll('.M_ArticleRightBlock')
+//   const contentPage = {
+//     id: '',
+//     image: contentCenterImage,
+//     title: contentNameTitle,
+//     description: contentDescription
+//   }
 
-  contentPageId.forEach((id) => {
-    content.forEach((contentPage) => {
-      if (contentPage.id === id) {
-      }
-    })
-  })
-}
+//   contentPageId.forEach((id) => {
+//     content.forEach((contentPage) => {
+//       if (contentPage.id === id) {
+//       }
+//     })
+//   })
+// }
 
 function initZodiaks() {
   getZodiakTags()
   getZodiakNames()
   const zodiaks = document.querySelectorAll('.A_ZodiakButton')
-
-  let zodiakClicked = zodiaks[0]
+  const zodiaksname = document.querySelectorAll('.A_ZodiakName')
+  const zodiaksinfo = document.querySelectorAll('.M_ZodiakRightBlock')
+  const zodiaksimage = document.querySelectorAll('.Q_ZodiakCenter')
 
   for (let i = 0; i < zodiaks.length; i++) {
-    zodiaks[i].addEventListener('click', function () {
-      zodiakClicked.classList.remove('active')
-      this.classList.add('active')
+    const buttons = zodiaks[i]
+    buttons.addEventListener('click', function () {
+      const button = buttons.dataset.tag
+      for (let i = 0; i < zodiaks.length; i++) {
+        const button = zodiaks[i]
+        button.classList.remove('active')
+      }
+      buttons.classList.add('active')
 
-      zodiakClicked = this
+      for (let i = 0; i < zodiaksname.length; i++) {
+        const names = zodiaksname[i]
+        names.classList.remove('active')
+        console.log(names.dataset.name, button)
+        if (names.dataset.name == button) {
+          zodiaksname[i].classList.add('active')
+        }
+      }
+
+      for (let i = 0; i < zodiaksinfo.length; i++) {
+        const info = zodiaksinfo[i]
+        info.classList.remove('active')
+        console.log(info.dataset.name, button)
+        if (info.dataset.name == button) {
+          zodiaksinfo[i].classList.add('active')
+        }
+      }
+
+      for (let i = 0; i < zodiaksimage.length; i++) {
+        const image = zodiaksimage[i]
+        image.classList.remove('active')
+        console.log(image.dataset.name, button)
+        if (image.dataset.name == button) {
+          zodiaksimage[i].classList.add('active')
+        }
+      }
     })
-  }
-
-  if (Array(zodiaks).classList.contains('active')) {
-    console.log('hi')
-    const clickedTag = zodiaks.dataset.tag.split()
-    console.log(clickedTag)
-
-    let foundName = names.filter((e) => {
-      return e === clickedTag
-    })
-    console.log(clickedTag)
-    zodiakName.classList.remove('active')
-    if ((foundName = clickedTag)) {
-      zodiakName.classList.add('active')
-    }
   }
 }
+
+// if (Array(zodiaks).classList.contains('active')) {
+//   const clickedTag = zodiaks.dataset.tag.split()
+//   console.log(clickedTag)
+
+//   let foundName = names.filter((e) => {
+//     return e === clickedTag
+//   })
+//   console.log(clickedTag)
+//   zodiakName.classList.remove('active')
+//   if ((foundName = clickedTag)) {
+//     zodiakName.classList.add('active')
+//   }
+// }
 
 document.addEventListener('DOMContentLoaded', () => {
   initZodiaks()
